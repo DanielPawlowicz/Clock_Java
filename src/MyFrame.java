bf95d7298d6995b4ab1039cabeefd0384b59aa0b
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-06 11:09:30 +0100

    Displaying current time

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,9 +1,17 @@
 import java.awt.FlowLayout;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MyFrame extends JFrame{
 
+	Calendar calendar;
+	SimpleDateFormat timeFormat;
+	JLabel timeLabel;
+	String time;
+	
 	MyFrame(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("My Clock");
@@ -11,6 +19,14 @@ public class MyFrame extends JFrame{
 		this.setSize(350,200);
 		this.setResizable(false);
 		
+		timeFormat = new SimpleDateFormat("hh:mm:ss a");
+		
+		timeLabel = new JLabel();
+		
+		time = timeFormat.format(Calendar.getInstance().getTime());
+		timeLabel.setText(time);
+		
+		this.add(timeLabel);
 		this.setVisible(true);
 	}
 	
